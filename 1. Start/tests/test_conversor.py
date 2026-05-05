8d9bd176fcc60a94278810470aa85552d01787a2
FehFialho <fernandaklechowicz@gmail.com>
2026-05-05 11:27:03 -0300

    feat: implemented converter functions and tests

--- 1. Start/tests/test_conversor.py
+++ 1. Start/tests/test_conversor.py
@@ -4,7 +4,7 @@ import os
 # Adiciona o diretório pai (Start) ao sys.path para poder importar Calculator
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from Conversor import fahToCels, celsToFah
+from Converter import fahToCels, celsToFah
 
 def test_fahToCels():
     assert fahToCels(32) == 0
