892415c46830571c7c3ab105dd6c177c8930cee6
FehFialho <fernandaklechowicz@gmail.com>
2026-05-05 09:25:31 -0300

    feat: added first functionst

---
+++ 1. Calculator.py
@@ -0,0 +1,20 @@
+
+
+def sum(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    if (b == 0):
+        print("Can't divide by 0!")
+        return
+    return a / b
+
+def multiply(a, b):
+    return a * b
+
+
+print(sum(1,2))
+print(divide(5,0))
